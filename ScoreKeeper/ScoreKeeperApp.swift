c00cf199092ddfb98e2869e1e86e21fa1a560c6c
egeckmk <egeckmk@gmail.com>
2025-04-04 22:42:07 +0300

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Ege Çakmak on 4.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
